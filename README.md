29ac6c020a867c0a1e868375ca7fd9900bbf086c
Site Mao <site.mao1992@gmail.com>
2018-03-18 15:12:06 -0700

    few additional updates

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # imgur-album-discord-bot
-Discord bot to automatically improve imgur albums
+Discord bot to automatically import imgur albums
+
+## Intro
+
+I wanted to learn how to build a discord bot and learn how to use react emojis to capture user inputs.
+I decided to go with implementing a discord bot that automatically parsed through an imgur album and
+displayed its images with a way to use discord emojis to scroll through them.
+
+Here's how the final result looks.
+
+![dog1](https://i.imgur.com/LCBOYyz.png "Example 1")
+![dog2](https://i.imgur.com/bTg8YYR.gifv "Example 2")
+
+Clicking on the emoji arrows scrolls through the images in the album.
+
+## Start
+
+1. Clone this repo
+2. Run `npm install`
+3. Register for the imgur API, and add the auth info to `auth.json`
+4. Register for the discord API, create a bot and add the auth info to `auth.json`
+5. Invite the bot to your discord server
+6. Run the bot with `node bot.js`
+7. Access album by typing the command `!album https://api.imgur.com/3/album/vtJCJ`
--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
-let Discord = require('discord.js')
+const Discord = require('discord.js');
 const client = new Discord.Client();
-let request = require('request');
+const request = require('request');
+const auth = require('./auth.json');
 
-let auth = require('./auth.json');
-
-const img = 'https://i.imgur.com/IcrST7s.jpg';
-const img2 = 'https://i.imgur.com/3ktz5Ql.jpg';
-const botname = 'site-bot';
+const botUsername = auth.discord.bot_name;
+const createImage = 'CREATE_IMG'
 const emojiList = {
     leftArrow: "⬅",
     rightArrow: "➡"
@@ -19,13 +17,10 @@ class Bot {
 
     initialize() {
         client.on('message', message => {
-            if (message.content.startsWith('album')) {
-                message.channel.send('CREATEIMG')
-                .then((sentMessage) => {
-                    this.populateAlbum(message, sentMessage);
-                });
+            if (message.content.startsWith('!')) {
+                this.handleCommand(message);
             }
-            if (message.author.username === botname) {
+            if (message.author.username === botUsername) {
                 this.handleBotMessage(message);
             }
         });
@@ -33,8 +28,22 @@ class Bot {
         client.login(auth.discord.auth_token);
     }
     
+    handleCommand(message) {
+        let input = message.content.split(' ');
+        if (input.length !== 2) {
+            return;
+        }
+
+        if (input[0] == '!album') {
+            message.channel.send(createImage)
+            .then((sentMessage) => {
+                this.populateAlbum(input[1], sentMessage);
+            });
+        }
+    }
+
     handleBotMessage(message) {
-        if (message.content !== 'CREATEIMG') {
+        if (message.content !== createImage) {
             return;
         }
         this.addReactions(message);
@@ -51,7 +60,8 @@ class Bot {
     }
 
     createReactionCollector(message) {
-        const filter = (reaction, user) => (reaction.emoji.name === emojiList.leftArrow || reaction.emoji.name === emojiList.rightArrow) && user.username !== botname;
+        const filter = (reaction, user) => (reaction.emoji.name === emojiList.leftArrow || reaction.emoji.name === emojiList.rightArrow)
+                                            && user.username !== botUsername;
         const collector = message.createReactionCollector(filter);
         collector.on('collect', r => this.emojiCollected(message, r.emoji.name));
     }
@@ -67,12 +77,12 @@ class Bot {
 
     handleLeftArrow(message) {
         let imagesData = this.albumDict[message.id];
-        console.log(imagesData);
         let numImages = imagesData.images.length;
         let current = imagesData.current;
         current = this.adjustImageIndex(current, -1, numImages);
         this.albumDict[message.id].current = current;
-        message.edit(imagesData.images[current].link);
+        this.editDisplayImage(message, imagesData.images[current].link);
+        // message.edit(imagesData.images[current].link);
     }
 
     handleRightArrow(message) {
@@ -81,7 +91,8 @@ class Bot {
         let current = imagesData.current;
         current = this.adjustImageIndex(current, 1, numImages);
         this.albumDict[message.id].current = current;
-        message.edit(imagesData.images[current].link);
+        this.editDisplayImage(message, imagesData.images[current].link);
+        // message.edit(imagesData.images[current].link);
     }
 
     adjustImageIndex(index, shift, modulo) {
@@ -90,17 +101,15 @@ class Bot {
         return final;
     }
 
+    editDisplayImage(message, link) {
+        message.edit(link);
+    }
+
     // TODO: Add support for galleries
     // const url = 'https://api.imgur.com/3/gallery/album/vgW1p';
     // const url2 = 'https://api.imgur.com/3/album/vtJCJ';
 
-    populateAlbum(message, sentMessage) {
-        let input = message.content.split(' ');
-        if (input.length !== 2) {
-            return;
-        }
-
-        let albumLink = input[1];
+    populateAlbum(albumLink, sentMessage) {
         this.retrieveImagesFromUrl(albumLink, sentMessage);
     }
 
@@ -117,7 +126,9 @@ class Bot {
                 return;
             }
             let parsed = JSON.parse(body);
+            // Set the default displayed image to the first image
             this.albumDict[sentMessage.id] = { images: parsed.data.images, current: 0 };
+            this.editDisplayImage(sentMessage, this.albumDict[sentMessage.id].images[0].link);
         });
     }
 
